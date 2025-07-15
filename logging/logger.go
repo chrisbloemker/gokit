606f24716ea1cb4684d05f1a5ed7b51b1b5e2a91
chrisbloemker <chrisbloemker@gmail.com>
2025-07-15 12:45:30 -0400

    add wrapper

--- logging/logger.go
+++ logging/logger.go
@@ -8,3 +8,12 @@ func NewLogger() *zap.Logger {
 	logger, _ := zap.NewProduction()
 	return logger
 }
+
+// Convenience wrapper for structured logging
+func ZapString(key, value string) zap.Field {
+	return zap.String(key, value)
+}
+
+func ZapError(err error) zap.Field {
+	return zap.Error(err)
+}
